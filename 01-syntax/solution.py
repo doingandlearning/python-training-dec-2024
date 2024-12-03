7e7ccf070cdc9d8858c7be6f78bb0fc1d0aa7e02
Kevin Cunningham <github@kevincunningham.co.uk>
2024-12-03 11:08:34 +0000

    adds lab

---
+++ 01-syntax/solution.py
@@ -0,0 +1,23 @@
+
+print(f"Sum is {1 + 3}")
+
+first_number = int(input("What is the first number? "))
+second_number = int(input("What is the second number? "))
+print(sum)
+sum = first_number + second_number
+print(sum)
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+print(f"The sum is { first_number + second_number}")
+print(f"The difference is {first_number - second_number}")
+print(f"The product is {product}")
+print(f"The quotient is {quotient}")
+
+print("The sum is " + str(sum))
+print("""
+
+It respects new line characters!
+
+""")
