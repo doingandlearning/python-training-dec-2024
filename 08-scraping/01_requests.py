c42b6b6806b6526c4d16fac3aa43ca1693d19c4f
Kevin Cunningham <github@kevincunningham.co.uk>
2024-12-04 15:27:13 +0000

    adds lab

--- 08-scraping/01_requests.py
+++ 08-scraping/01_requests.py
@@ -10,3 +10,14 @@ if response.status_code == 200:
     print(response.content[:500])
 else:
     print(f"Failed to fetch page. Status code {response.status_code}")
+
+url = "https://swapi.dev/api/people"
+
+response = requests.get(url)
+print("Here i am :) ")
+print(response)
+if response.status_code == 200:
+    print("Page fetched successfully!")
+    print(response.content[:500])
+else:
+    print(f"Failed to fetch page. Status code {response.status_code}")
---
+++ 08-scraping/02_beautifulsoup.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+
+html_content = """
+<html>
+    <head>
+        <title>Example Page</title>
+    </head>
+    <body>
+        <h1>Welcome to Example.com</h1>
+        <p>This is a paragraph of text.</p>
+        <a href="https://example.com/page2">Link to page 2</a>
+    </body>
+</html>
+"""
